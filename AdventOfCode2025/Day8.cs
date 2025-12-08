62e6521d07fed107f8bfd59842c94200bcc91525
Waterlok653 <yoann@dediesbach.ch>
2025-12-08 09:46:11 +0100

    do day 8

--- AdventOfCode2025/Day8.cs
+++ AdventOfCode2025/Day8.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,7 +22,6 @@ namespace AdventOfCode2025
             string input = System.IO.File.ReadAllText("C:\\Users\\Moi\\source\\repos\\AdventOfCode2025\\AdventOfCode2025\\inputs\\day8.txt");
             string[] row = input.Split(new[] { "\r\n", "\n\r", "\r", "\n" }, StringSplitOptions.None);
             Day8Object[] boxs = new Day8Object[row.Length];
-            long numberOfJoinTo = 1000;
 
             for (long i = 0; i < row.Length; i++)
             {
@@ -33,69 +33,47 @@ namespace AdventOfCode2025
                 boxs[i].groupe = 0;
                 boxs[i].connectedTo = new List<Day8Object>();
             }
+            List<(Day8Object, Day8Object, double)> allLinke = new List<(Day8Object, Day8Object, double)>();
 
-            long nbGroupe = 1;
-            int timeDone = 0;
-            do
+            for (int i = 0; i < boxs.Length; i++)
             {
-                Day8Object closestOne = new Day8Object();
-                Day8Object closestSeconde = new Day8Object();
-                double distance = double.MaxValue;
-                bool isSameGroupe = false;
-                foreach (var box in boxs)
+                for (int j = i + 1; j < boxs.Length; j++)
                 {
-                    foreach (var boxToConnect in boxs)
-                    {
-                        //if its the same or are alredy linked
-                        if (box == boxToConnect || (box.connectedTo.Contains((boxToConnect))))
-                        {
-                            continue;
-                        }
-                        //get smalest distance
-                        var currentDistance = Math.Sqrt((box.y - boxToConnect.y) * (box.y - boxToConnect.y) + (box.x - boxToConnect.x) * (box.x - boxToConnect.x) + (box.z - boxToConnect.z) * (box.z - boxToConnect.z));
-                        if (distance > currentDistance)
-                        {
-                            distance = currentDistance;
-                            closestOne = box;
-                            closestSeconde = boxToConnect;
-                            if (box.groupe != 0 && box.groupe == boxToConnect.groupe)
-                            {
-                                isSameGroupe = true;
-                            }
-                            else
-                            {
-                                isSameGroupe = false;
-                            }
-                        }
-
-                    }
+                    allLinke.Add((boxs[i], boxs[j], (boxs[i].y - boxs[j].y) * (boxs[i].y - boxs[j].y) + (boxs[i].x - boxs[j].x) * (boxs[i].x - boxs[j].x) + (boxs[i].z - boxs[j].z) * (boxs[i].z - boxs[j].z)));
                 }
-                if (!isSameGroupe)
-                {
-                    //Merge Groupe
-                    var boxToChange = boxs.Where(b => b.groupe != 0 && (b.groupe == closestSeconde.groupe || b.groupe == closestOne.groupe)).ToArray();
-                    foreach (var item in boxToChange)
-                    {
-                        item.groupe = nbGroupe;
-                    }
-                    closestOne.groupe = nbGroupe;
-                    closestSeconde.groupe = nbGroupe;
-
-                    nbGroupe++;
-                }
-                //say that they are linked
-                closestOne.connectedTo.Add(closestSeconde);
-                closestSeconde.connectedTo.Add(closestOne);
-                Console.WriteLine(timeDone);
-                timeDone++;
             }
-            while (timeDone < numberOfJoinTo);
-
-            //size of every groupe
-            List<long> sizeGroupe = new List<long>();
-            for (long i = 1; i <= nbGroupe; i++)
+            List<(Day8Object, Day8Object, double)> linkToUse = allLinke.OrderBy(t => t.Item3).Take(1000).ToList();
+            int currentGroupe = 1;
+            foreach (var link in linkToUse)
             {
-                sizeGroupe.Add(boxs.Where(b => b.groupe == i).Count());
+                //Merge Groupe
+                var boxToChange = boxs.Where(b => b.groupe != 0 && (b.groupe == link.Item2.groupe || b.groupe == link.Item1.groupe)).ToArray();
+                foreach (var item in boxToChange)
+                {
+                    item.groupe = currentGroupe;
+                }
+                link.Item1.groupe = currentGroupe;
+                link.Item2.groupe = currentGroupe;
+
+                currentGroupe++;
+            }
+            //size of every groupe
+            List<Day8Object> allPoint = new List<Day8Object>();
+            foreach (var item in linkToUse)
+            {
+                if (!allPoint.Contains(item.Item1))
+                {
+                    allPoint.Add(item.Item1);
+                }
+                if (!allPoint.Contains(item.Item2))
+                {
+                    allPoint.Add(item.Item2);
+                }
+            }
+            List<long> sizeGroupe = new List<long>();
+            for (long i = 1; i <= currentGroupe; i++)
+            {
+                sizeGroupe.Add(allPoint.Where(b => b.groupe == i).Count());
             }
 
             var top3 = sizeGroupe.OrderDescending().Take(3).ToArray();
@@ -107,27 +85,58 @@ namespace AdventOfCode2025
                 }
             }
 
-            //test
-            /*foreach (var item in boxs.GroupBy(t => t.groupe))
-            {
-                Console.WriteLine(item.Key);
-                foreach (var t in item)
-                {
-                    Console.Write(t.x + "," + t.y + "," + t.z + "|");
-                }
-                Console.WriteLine();
-                Console.WriteLine();
-            }*/
-
             Console.WriteLine("The Top5: " + top3[0] * top3[1] * top3[2]);
-
         }
 
 
         public static void SolveTwo()
         {
-            string input = System.IO.File.ReadAllText("C:\\Users\\Moi\\source\\repos\\AdventOfCode2025\\AdventOfCode2025\\inputs\\day8.test.txt");
+            string input = System.IO.File.ReadAllText("C:\\Users\\Moi\\source\\repos\\AdventOfCode2025\\AdventOfCode2025\\inputs\\day8.txt");
             string[] row = input.Split(new[] { "\r\n", "\n\r", "\r", "\n" }, StringSplitOptions.None);
+            Day8Object[] boxs = new Day8Object[row.Length];
+
+            for (long i = 0; i < row.Length; i++)
+            {
+                var pos = row[i].Split(",").ToArray();
+                boxs[i] = new Day8Object();
+                boxs[i].x = long.Parse(pos[0]);
+                boxs[i].y = long.Parse(pos[1]);
+                boxs[i].z = long.Parse(pos[2]);
+                boxs[i].groupe = 0;
+                boxs[i].connectedTo = new List<Day8Object>();
+            }
+            List<(Day8Object, Day8Object, double)> allLinke = new List<(Day8Object, Day8Object, double)>();
+
+            for (int i = 0; i < boxs.Length; i++)
+            {
+                for (int j = i + 1; j < boxs.Length; j++)
+                {
+                    allLinke.Add((boxs[i], boxs[j], (boxs[i].y - boxs[j].y) * (boxs[i].y - boxs[j].y) + (boxs[i].x - boxs[j].x) * (boxs[i].x - boxs[j].x) + (boxs[i].z - boxs[j].z) * (boxs[i].z - boxs[j].z)));
+                }
+            }
+            List<(Day8Object, Day8Object, double)> linkToUse = allLinke.OrderBy(t => t.Item3).ToList();
+            int currentGroupe = 1;
+            foreach (var link in linkToUse)
+            {
+                //Merge Groupe
+                var boxToChange = boxs.Where(b => b.groupe != 0 && (b.groupe == link.Item2.groupe || b.groupe == link.Item1.groupe)).ToArray();
+
+                foreach (var item in boxToChange)
+                {
+                    item.groupe = currentGroupe;
+                }
+                link.Item1.groupe = currentGroupe;
+                link.Item2.groupe = currentGroupe;
+
+                if (boxs.GroupBy(b => b.groupe).Count() == 1)
+                {
+                    Console.WriteLine("the producte of last linke is " +link.Item1.x * link.Item2.x);
+                    break;
+                }
+
+                currentGroupe++;
+            }
+
 
         }
     }
