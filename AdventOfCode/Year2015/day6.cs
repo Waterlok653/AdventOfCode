0892089e9e1756dc3aaaccce46be71cb5ac126a3
Waterlok653 <yoann@dediesbach.ch>
2026-02-10 22:44:31 +0100

    do day 6.2 2015

--- AdventOfCode/Year2015/day6.cs
+++ AdventOfCode/Year2015/day6.cs
@@ -39,7 +39,7 @@ namespace AdventOfCode.Year2015
             {
                 for (int j = 0; j < 1000; j++)
                 {
-                    if (table[i,j])
+                    if (table[i, j])
                     {
                         count++;
                     }
@@ -55,7 +55,35 @@ namespace AdventOfCode.Year2015
             string input = File.ReadAllText(configFile);
             string[] inputs = input.Split(new[] { "\r\n", "\n\r", "\r", "\n" }, StringSplitOptions.None);
 
-            return 0;
+            int[,] table = new int[1000, 1000];
+            foreach (string line in inputs)
+            {
+                var match = Regex.Match(line, @"(\d+,\d+).+\b(\d+,\d+)");
+                var start = match.Groups[1].Value.Split(',');
+                var end = match.Groups[2].Value.Split(",");
+                for (int i = int.Parse(start[0]); i <= int.Parse(end[0]); i++)
+                {
+                    for (int j = int.Parse(start[1]); j <= int.Parse(end[1]); j++)
+                    {
+                        switch (line[6])
+                        {
+                            case ' ': table[i, j] += 2; break;
+                            case 'f': if (table[i, j] != 0) table[i, j]--; break;
+                            case 'n': table[i, j]++; break;
+                        }
+                    }
+                }
+            }
+            long count = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                for (int j = 0; j < 1000; j++)
+                {
+                    count += table[i, j];
+
+                }
+            }
+            return count;
         }
     }
 }
