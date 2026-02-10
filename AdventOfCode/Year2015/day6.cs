c00840a5f5d13f3b7c0450ba9194a38b5f45d74b
Waterlok653 <yoann@dediesbach.ch>
2026-02-10 22:40:38 +0100

    do day 6.1 2015

--- AdventOfCode/Year2015/day6.cs
+++ AdventOfCode/Year2015/day6.cs
@@ -2,6 +2,7 @@
 using System;
 using System.IO;
 using System.Numerics;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Year2015
 {
@@ -14,7 +15,37 @@ namespace AdventOfCode.Year2015
             string input = File.ReadAllText(configFile);
             string[] inputs = input.Split(new[] { "\r\n", "\n\r", "\r", "\n" }, StringSplitOptions.None);
 
-            return 0;
+            bool[,] table = new bool[1000, 1000];
+            foreach (string line in inputs)
+            {
+                var match = Regex.Match(line, @"(\d+,\d+).+\b(\d+,\d+)");
+                var start = match.Groups[1].Value.Split(',');
+                var end = match.Groups[2].Value.Split(",");
+                for (int i = int.Parse(start[0]); i <= int.Parse(end[0]); i++)
+                {
+                    for (int j = int.Parse(start[1]); j <= int.Parse(end[1]); j++)
+                    {
+                        switch (line[6])
+                        {
+                            case ' ': table[i, j] = !table[i, j]; break;
+                            case 'f': table[i, j] = false; break;
+                            case 'n': table[i, j] = true; break;
+                        }
+                    }
+                }
+            }
+            var count = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                for (int j = 0; j < 1000; j++)
+                {
+                    if (table[i,j])
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
         }
 
         public BigInteger SolveTwo()
