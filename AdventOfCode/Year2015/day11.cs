d97606aa98e018d49b864376a8939c2596d1b874
Waterlok653 <yoann@dediesbach.ch>
2026-02-11 19:38:39 +0100

    do day 11 fo 2015

--- AdventOfCode/Year2015/day11.cs
+++ AdventOfCode/Year2015/day11.cs
@@ -2,29 +2,92 @@
 using System;
 using System.IO;
 using System.Numerics;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Year2015
 {
     public class Day11
     {
-        public BigInteger SolveOne()
+        public string SolveOne()
         {
             string basePath = Environment.GetEnvironmentVariable("ADVENT_OF_CODE");
             string configFile = Path.Combine(basePath, "AdventOfCode\\Year2015\\inputs\\day11.txt");
-            string input = File.ReadAllText(configFile);
-            string[] inputs = input.Split(new[] { "\r\n", "\n\r", "\r", "\n" }, StringSplitOptions.None);
+            char[] input = File.ReadAllText(configFile).ToCharArray();
+            input = GetNextPassword(input);
 
-            return 0;
+            return new string(input);
         }
 
-        public BigInteger SolveTwo()
+        private char[] GetNextPassword(char[] input)
+        {
+            bool isPasswordCorrect = false;
+            do
+            {
+                var indexOfInvalid = input.IndexOfAny(new char[] { 'i', 'o', 'l' });
+                if (indexOfInvalid != -1)
+                {
+                    input[indexOfInvalid]++;
+                    for (int i = indexOfInvalid + 1; i < input.Length; i++)
+                    {
+                        input[i] = 'a';
+                    }
+                    continue;
+                }
+                bool hasASuite = false;
+                for (int i = 0; i < input.Length - 2; i++)
+                {
+                    if (input[i] + 1 == input[i + 1] && input[i] + 2 == input[i + 2])
+                    {
+                        hasASuite = true;
+                        break;
+                    }
+                }
+                if (!hasASuite)
+                {
+                    input = IncrementPasswordOne(input);
+                    continue;
+                }
+
+                var match = Regex.Matches(new string(input), @"([a-zA-Z])\1");
+
+                if (match.GroupBy(t => t.Value).Count() < 2)
+                {
+                    input = IncrementPasswordOne(input);
+                    continue;
+                }
+                isPasswordCorrect = true;
+            } while (!isPasswordCorrect);
+            return input;
+        }
+
+        private char[] IncrementPasswordOne(char[] mdp)
+        {
+            for (int i = mdp.Length - 1; i >= 0; i--)
+            {
+                if (mdp[i] == 'z')
+                {
+                    mdp[i] = 'a';
+                    continue;
+                }
+                else
+                {
+                    mdp[i]++;
+                    break;
+                }
+            }
+            return mdp;
+        }
+        public string SolveTwo()
         {
             string basePath = Environment.GetEnvironmentVariable("ADVENT_OF_CODE");
             string configFile = Path.Combine(basePath, "AdventOfCode\\Year2015\\inputs\\day11.txt");
-            string input = File.ReadAllText(configFile);
-            string[] inputs = input.Split(new[] { "\r\n", "\n\r", "\r", "\n" }, StringSplitOptions.None);
+            char[] input = File.ReadAllText(configFile).ToCharArray();
 
-            return 0;
+            input = GetNextPassword(input);
+            input = IncrementPasswordOne(input);
+            input = GetNextPassword(input);
+
+            return new string(input);
         }
     }
 }
