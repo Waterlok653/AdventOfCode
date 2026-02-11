26696399aa82b080d2833c1c8a20a66351d5ead8
Waterlok653 <yoann@dediesbach.ch>
2026-02-11 11:32:02 +0100

    do day 8 of 2015

--- AdventOfCode/Year2015/day8.cs
+++ AdventOfCode/Year2015/day8.cs
@@ -1,7 +1,9 @@
 
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Numerics;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Year2015
 {
@@ -14,7 +16,16 @@ namespace AdventOfCode.Year2015
             string input = File.ReadAllText(configFile);
             string[] inputs = input.Split(new[] { "\r\n", "\n\r", "\r", "\n" }, StringSplitOptions.None);
 
-            return 0;
+            var sum = 0;
+            foreach (var line in inputs)
+            {
+                sum += 2;
+                var match = Regex.Matches(line, @"(\\\\)|(\\"")|(\\x)");
+                sum += match.Count;
+                sum += match.Count(t => t.Value.Equals("\\x")) * 2;
+            }
+
+            return sum;
         }
 
         public BigInteger SolveTwo()
@@ -24,7 +35,16 @@ namespace AdventOfCode.Year2015
             string input = File.ReadAllText(configFile);
             string[] inputs = input.Split(new[] { "\r\n", "\n\r", "\r", "\n" }, StringSplitOptions.None);
 
-            return 0;
+            var sum = 0;
+            foreach (var line in inputs)
+            {
+                sum += 4;
+                var match = Regex.Matches(line, @"(\\\\)|(\\"")|(\\x)");
+                sum += match.Count * 2;
+                sum -= match.Count(t => t.Value.Equals("\\x"));
+            }
+
+            return sum;
         }
     }
 }
